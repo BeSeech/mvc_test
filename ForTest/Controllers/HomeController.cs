a665aa6b149bc30b26ceb419d15e2b18c3a9dbb3
beseech <beseechmv@evos.com.ua>
2017-09-11 14:47:08 +0300

    Table

---
+++ ForTest/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ForTest.Models;
+
+namespace ForTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View(SimpleRepository.SharedRepository.Products);
+        }
+    }
+}
