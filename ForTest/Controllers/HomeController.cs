417aca661a2ca189319e4c7446bf3e7dcf7b931d
beseech <beseechmv@evos.com.ua>
2017-09-12 18:03:03 +0300

    Pretesting state

--- ForTest/Controllers/HomeController.cs
+++ ForTest/Controllers/HomeController.cs
@@ -6,10 +6,22 @@ namespace ForTest.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
         // GET
         public IActionResult Index()
         {
-            return View(SimpleRepository.SharedRepository.Products);
+            return View(Repository.Products.Where( p => p?.Price < 50));
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
         }
     }
 }
